c0094ed602668f845f5d4178f3065617105fbd67
Bryan Mytko <bryanmytko@gmail.com>
2022-01-05 05:54:03 -0500

    Build out the number fill in functionality

--- src/components/digits.component.js
+++ src/components/digits.component.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import Nouislider from "nouislider-react";
 import "nouislider/distribute/nouislider.css";
+import { useKey } from "rooks";
 
 const DEFAULT_LENGTH = 10;
 
@@ -12,7 +13,11 @@ const Digits = () => {
   const [min, setMin] = useState(20);
   const [max, setMax] = useState(80);
   const [length, setLength] = useState(DEFAULT_LENGTH);
+  const [showReady, setShowReady] = useState(false);
+  const [showFillIn, setShowFillIn] = useState(false);
   const [randomNumber, setRandomNumber] = useState('');
+  const [fillInNumber, setFillInNumber] = useState('_');
+  const [fillInNumberPosition, setFillInNumberPosition] = useState(0);
 
   const onSlide = (range) => {
     setMin(parseInt(range[0]));
@@ -27,8 +32,40 @@ const Digits = () => {
       randomNumbers.push(n.toString().padStart(2,'0'));
     }
 
+    setShowReady(true);
     setRandomNumber(randomNumbers.join(''));
   }
+
+  const hideNumber = () => {
+    setShowFillIn(true);
+    setFillInNumber("_");
+  }
+
+  /* @TODO we need to have an and start end state that:
+  *  1. Hides the irrelevant UI components
+  *  2. Displays x/y result when fill in is complete
+  *  3. Maybe also break some of this stuff up and refactor?
+  */
+  const updateFillInNumber = (e) => {
+    if(!showFillIn) return;
+    if(fillInNumberPosition > randomNumber.length){
+      setShowFillIn(false); // temporary
+    }
+
+    const key = e.key;
+    const index = fillInNumberPosition;
+
+    if(key === randomNumber[fillInNumberPosition]){
+      const newFillInNumber = (fillInNumber + randomNumber[fillInNumberPosition]).replace(/_/, '');
+      setFillInNumber(newFillInNumber);
+    } else {
+      setFillInNumber(`${fillInNumber.replace(/_/, '')}<span class="red">${randomNumber[fillInNumberPosition]}</span>`);
+    }
+
+    setFillInNumberPosition(index + 1);
+  }
+
+  useKey([0,1,2,3,4,5,6,7,8,9], updateFillInNumber);
 
   return <>
     <h5>Digits</h5>
@@ -38,7 +75,7 @@ const Digits = () => {
         <div className="row">Max: <label id="max">{max}</label></div>
         <div className="row inline">
           <label>Length:</label>
-          <div class="input-field inline">
+          <div className="input-field inline">
             <input type="text" value={length} className="align-right" onChange={e => setLength(e.target.value)} />
           </div>
         </div>
@@ -48,8 +85,14 @@ const Digits = () => {
         <div className="row">
           <button className="btn" onClick={generateNumber}>Generate Number</button>
         </div>
-        <div className="row">
-          <input id="randomNumber" value={randomNumber} />
+        <div className={`row ${showFillIn ? 'hide' : '' }`}>
+          <h1>{randomNumber}</h1>
+        </div>
+        <div className={`row ${showFillIn ? '' : 'hide' }`}>
+          <h1 dangerouslySetInnerHTML={{__html: fillInNumber}}></h1>
+        </div>
+        <div className={`row ${showReady ? '' : 'hide' }`}>
+          <button className="btn" onClick={hideNumber}>Ready?</button>
         </div>
       </div>
     </form>
